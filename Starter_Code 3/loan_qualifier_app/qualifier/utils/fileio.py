ab863451ceafc941097cf9d33868b4b2b909a7a2
sterling-d <105071493+sterling-d@users.noreply.github.com>
2022-06-25 11:56:54 -0700

    Extended the "save_csv" function
    Used an "if" statement within save_csv function.

---
+++ Starter_Code 3/loan_qualifier_app/qualifier/utils/fileio.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+
+import csv
+
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+
+def save_csv(qualifying_loans):
+
+    data = []
+
+    header = ["Lender","Max Loan Amount","Max LTV","Max DTI","Min Credit Score","Interest Rate"]
+
+    for dataloans in qualifying_loans:
+
+        questionary.form(
+
+        firstquestion = questionary.confirm ("Would you like to save your qualifying loans?", default=True),
+        secondquestion = questionary.select ("Select option: ", choices=["Yes", "No"] )
+        
+        ).ask()
+
+        if choices in secondquestion == ["Yes"]:
+
+            return data 
+
+        else: 
+
+            print ()
+
+            
+            
+            
+            
+            
+# with open ('path/to/daily_rate_sheet.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(row)
+
+
+
+        else: print "K bye. "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    saving_loan = questionary.form(
+
+        firstquestion = questionary.confirm ("Would you like to save your qualifying loans?", default=True),
+        secondquestion = questionary.select ("Select option: ", choices=["Yes", "No"] )
+        
+        ).ask()
+
+    
+
+    csv_question = input ("Would you like to save your loan qualifier application to a CSV file?")
+
+    if csv_promt  
+
+    with open ('path/to/daily_rate_sheet.csv', 'w') as f:
+
+        writer = csv.writer(f)
+
+        writer.writerow(row)
+
+
+    
+    
+    
